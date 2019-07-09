ec50d9c3901d46547d95aa23cefab9d401deecd0
1 <2>
2019-07-09 17:33:43 +0800

    fd

--- git_push.sh
+++ git_push.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 message=${@:1}
 echo $message
+MYDIR=`dirname $0`
 if [ -z "$message" ]
 then 
 	echo "please input message!"
 	exit
 fi
 echo "===============git adding==============="
-git add .
+git add $MYDIR
 echo "=============git add completed=============="
 
 echo "===============git commiting==============="
